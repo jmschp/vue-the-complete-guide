a423a8b4d4514f07c98085ff61514e8324f8a5cd
Miguel Hargreaves Pimenta <jmiguelpimenta@icloud.com>
2024-09-21 15:30:12 +0100

    refactor: rmeove unnecessary files

--- coach-finder/src/router/index.js
+++ coach-finder/src/router/index.js
@@ -1,38 +1,29 @@
 import { createRouter, createWebHistory } from "vue-router"
+import CoachDetails from "@/pages/coaches/CoachDetails.vue"
+import CoachesList from "@/pages/coaches/CoachesList.vue"
+import CoachRegistration from "@/pages/coaches/CoachRegistration.vue"
+import ContactCoach from "@/pages/requests/ContactCoach.vue"
+import RequestReceived from "@/pages/requests/RequestReceived.vue"
+import NotFound from "@/pages/NotFound.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: "/",
-      redirect: "/coaches"
-    },
-    {
-      path: "/coaches",
-      component: null
-    },
+    { path: "/", redirect: "/coaches" },
+    { path: "/coaches", component: CoachesList },
     {
       path: "/coaches/:id",
-      component: null,
+      component: CoachDetails,
       children: [
         {
           path: "contact",
-          component: null
+            component: ContactCoach
         }
       ]
     },
-    {
-      path: "/register",
-      component: null
-    },
-    {
-      path: "/request",
-      component: null
-    },
-    {
-      path: "/:notFount(.*)",
-      component: null
-    }
+    { path: "/register", component: CoachRegistration },
+    { path: "/request", component: RequestReceived },
+    { path: "/:notFount(.*)", component: NotFound }
   ]
 })
 
